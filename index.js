80d5267850f04ad5b3c583bf20389c789a6aa64a
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-11 03:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,11 +26,3 @@ function takeANumber(line, name) {
 
   return `Welcome, ${name}. You are number ${line.length} in line.`
 };
-
-
-function nowServing(line){
-  if (!line.length){
-    return ("There is nobody waiting to be served!")
-  }
-  return "Currently serving ${line.shift()}."
-};
