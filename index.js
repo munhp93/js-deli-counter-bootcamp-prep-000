a19e1c38da3d37acf26753fd4f9395a766abcc4f
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-11 03:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function currentLine(line) {
   return `The line is currently: ${numbersAndNames.join(', ')}`
 };
 
-//function nowServing(line) {
-//  if (!line.length) {
-//    return "There is nobody waiting to be served!"
-//  }
+function nowServing(line) {
+  if (!line.length) {
+        return "There is nobody waiting to be served!"
+ }
 
-//  return `Currently serving ${line.shift()}.`
-//};
+ return `Currently serving ${line.shift()}.`
+};
 
 function takeANumber(line, name) {
   line.push(name)
