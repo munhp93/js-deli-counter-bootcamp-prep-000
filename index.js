3cc2f348af30049c1e446f6bb604da10bae78fc3
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-11 03:14:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ function nowServing(line){
   if (!line.length){
     return ("There is nobody waiting to be served!")
   }
-  return "Currently serving ${line.shift()}"
+  return "Currently serving ${line.shift()}."
 };
